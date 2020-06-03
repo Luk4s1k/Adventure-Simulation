533296795f515c42c27edb0cd6511f30aeeb642f
Luka Mitrovic <mitrovichluke@gmail.com>
2020-06-03 16:03:41 +0200

    enemies updated

--- src/main/java/simulation/enemies/Enemy.java
+++ src/main/java/simulation/enemies/Enemy.java
@@ -1,4 +1,17 @@
 package simulation.enemies;
 
+import java.util.Random;
+
 public abstract class Enemy {
+    protected int level;
+    protected int health;
+
+    public Enemy(){
+        Random rand = new Random();
+        this.level = rand.nextInt(11);
+        this.health = this.level * 2;
+    }
+
+    public abstract int getLevel();
+    public abstract int getHealth();
 }
--- src/main/java/simulation/enemies/Goblin.java
+++ src/main/java/simulation/enemies/Goblin.java
@@ -1,4 +1,21 @@
 package simulation.enemies;
+import java.util.Random;
 
-public class Goblin {
+public class Goblin extends Enemy{
+
+
+    public Goblin(){
+        System.out.println("Goblin was spawned");
+    }
+
+
+    @Override
+    public int getLevel() {
+        return this.level;
+    }
+
+    @Override
+    public int getHealth() {
+        return this.health;
+    }
 }
